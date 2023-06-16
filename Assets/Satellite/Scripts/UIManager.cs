4b45503abe7c03ea52456a2e9c6427a07692b6e8
Mark Bennison <mark.bennison@live.co.uk>
2023-06-16 14:11:37 +0100

    Added Time

--- Assets/Satellite/Scripts/UIManager.cs
+++ Assets/Satellite/Scripts/UIManager.cs
@@ -1,17 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject Satellite0View, Satellite1View;
+    TMPro.TextMeshProUGUI TimeTMP;
 
-    private void Start()
+	[SerializeField]
+	GameObject Satellite0View, Satellite1View;
+
+	private void Start()
     {
         Satellite0View.SetActive(true);
         Satellite1View.SetActive(false);
     }
+
+    private void Update()
+    {
+        TimeTMP.text = DateTime.Now.ToString("HH:mm:ss");
+
+	}
 
     public void SatelliteSelection(int index)
     {
